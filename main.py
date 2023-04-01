ea8681570fd6039848eed4064f7bee34dedabc16
Hshsbs37 <129624595+Hshsbs37@users.noreply.github.com>
2023-04-02 02:49:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -140,6 +140,7 @@ def api_hello():
 
     return jsonify(t)
 
-if __name__ == '__main__':
+if __name__ == '__main__':  
 
-    app.run(debug=True,port=os.getenv("PORT", default=5000))
+    app.run(host='0.0.0.0', port=80)
+
