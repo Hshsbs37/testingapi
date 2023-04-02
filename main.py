129360bcbb1274fe0cee517d3f03a8280a6c774c
Hshsbs37 <129624595+Hshsbs37@users.noreply.github.com>
2023-04-02 08:59:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -142,5 +142,5 @@ def api_hello():
 
 if __name__ == '__main__':  
 
-    app.run(debug=True,port=os.getenv("PORT",default=5000),host=0.0.0.0)
+    app.run(debug=True,host=0.0.0.0,port=os.getenv("PORT",default=5000))
 
